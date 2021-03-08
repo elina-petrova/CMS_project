8236f8a0a4c56b14549eb336e9f86264fd5695fc
elina <e_petrova@fanshaweonline.ca>
2021-03-08 17:35:51 -0500

    dot back

--- load.php
+++ load.php
@@ -4,7 +4,7 @@ define('ADMIN_PATH', ABSPATH.'/admin');
 define('ADMIN_SCRIPT_PATH', ADMIN_PATH.'/scripts');
 
 session_start();
-require_once ABSPATH.'/config/database.php';
+require_once ABSPATH.'./config/database.php';
 require_once  ADMIN_SCRIPT_PATH.'/read.php';
 require_once  ADMIN_SCRIPT_PATH.'/login.php';
 require_once  ADMIN_SCRIPT_PATH.'/functions.php';
